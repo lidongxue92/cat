3cade164723f87bd19fc7250b934a701c1f5061f
Dana <1475641137@qq.com>
2018-06-19 11:27:44 +0800

    6.19

---
+++ pages/list/eat.js
@@ -0,0 +1,77 @@
+// pages/list/eat.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // text:"这是一个页面"
+    eatlist: [
+      { title: '猫咪饮食小贴士' }
+    ],
+    imgUrls: [
+      '../../images/miao1.jpg',
+      '../../images/miao2.jpg',
+      '../../images/miao3.jpg',
+      '../../images/miao4.jpg'
+      // 'http://zxpic.gtimg.com/infonew/0/wechat_pics_-214279.jpg/168',
+      // 'http://zxpic.gtimg.com/infonew/0/wechat_pics_-214521.jpg/168'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
